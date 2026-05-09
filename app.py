1f9579e40a90a224cb5ff2987eb45367141a28a6
Ashwani Kumar Singh <91384674+Ashwanimorningstar@users.noreply.github.com>
2026-05-10 04:08:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from huggingface_hub import InferenceClient
+from PIL import Image
+import datetime
+client=InferenceClient(token="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")
+MODEL="stabilityai/stable-diffusion-xl-base-1.0"
+st.set_page_config(page_title="My Image Generator")
+st.write("Describe your image")
+prompt = st.text_input("Enter image prompt")
+if st.button("generate"):
+    with st.spinner("Image is genrating.."):
+        image=client.text_to_image(prompt,model=MODEL)
+        image.save("myimage.png")
+        st.success("image is saved")
